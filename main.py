71c358e776f96da848d2fe07ee0ad8862b324bbf
Xizhen Ma <xizhenma@XizhendeMBP.modem>
2020-09-08 09:38:59 +1000

    add read excel

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 import sys, datetime
+import xlrd
+
+
+def read_excel():
+    wb = xlrd.open_workbook('address.xlsx')# 打开Excel文件
+    sheet = wb.sheet_by_name('1')#通过excel表格名称(rank)获取工作表
+    dat = []  #创建空list
+    for a in range(sheet.nrows):  #循环读取表格内容（每次读取一行数据）
+        cells = sheet.row_values(a)  # 每行数据赋值给cells
+        data = str(cells)#因为表内可能存在多列数据，0代表第一列数据，1代表第二列，以此类推
+        dat.append(data) #把每次循环读取的数据插入到list
+    return dat
+
 
 
 def main(p_argv):
-    print("hello world")
-
+    a = read_excel()  # 返回整个函数的值
+    for b in a:  # 循环读取a变量list
+        print(b)
     return 0
 
 
