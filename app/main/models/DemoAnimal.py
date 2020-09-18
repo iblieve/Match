a3baea1be37dad04b8efe948366ab889d34ff07a
Xizhen Ma <xizhenma@XizhendeMBP.modem>
2020-09-18 15:59:00 +1000

    了解抽象类

--- app/main/models/DemoAnimal.py
+++ app/main/models/DemoAnimal.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
 
-class DemoAnimal:
-    name = None
 
-    def __init__(self, p_name):
-        self.name = p_name
-
+class DemoAnimal(metaclass=ABCMeta):
+    @abstractmethod
     def speak(self):
         pass
 
+    @abstractmethod
     def walk(self):
         """
         该方法会被其所有子类继承, 子类无需实现该函数即可调用到父类的walk方法
         :return:
         """
-        print('我会走')
+        # print('我会走')
+        pass
+
+
