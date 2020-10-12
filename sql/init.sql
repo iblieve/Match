9a924cf6b4a2ac6d88328b07399a587ea3f53e33
Xizhen Ma <xizhenma@XizhendeMBP.modem>
2020-10-12 18:56:08 +1100

    create sql and tabel

--- sql/init.sql
+++ sql/init.sql
@@ -1,24 +1,23 @@
 #
-CREATE DATABASE IF NOT EXISTS `user-center` DEFAULT CHARACTER SET = utf8mb4;
-Use `user-center`;
+CREATE DATABASE IF NOT EXISTS `mysql` DEFAULT CHARACTER SET = utf8mb4;
+Use `mysql`;
 
 #
-# Structure for table "sys_menu"
+# Structure for table "tpoint"
 #
 
-DROP TABLE IF EXISTS `sys_menu`;
-CREATE TABLE `sys_menu` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `parent_id` int(11) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `url` varchar(1024) DEFAULT NULL,
-  `path` varchar(1024) DEFAULT NULL,
-  `css` varchar(32) DEFAULT NULL,
-  `sort` int(11) NOT NULL,
-  `create_time` datetime NOT NULL,
-  `update_time` datetime NOT NULL,
-  `is_menu` int(11) DEFAULT NULL COMMENT '是否菜单 1 是 2 不是',
-  `hidden` int(11) DEFAULT NULL COMMENT '是否隐藏,0 false 1 true',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `tpoint`;
+CREATE TABLE `tpoint` (
+  `row_id` int(10) NOT NULL AUTO_INCREMENT,
+  `location_id` varchar(20) NOT NULL,
+  `province_name` varchar(30) NOT NULL,
+  `city_name` varchar(30) NOT NULL,
+  `district_name` varchar(30) DEFAULT NULL,
+  `town_name` varchar(30) DEFAULT NULL,
+  `location_name` varchar(300) NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `longitude` double(9,6) NOT NULL,
+  `latitude` double(9,6) NOT NULL,
+  PRIMARY KEY (`row_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
