b841e3736b24f48bdd455d69881a579586202f76
Xizhen Ma <xizhenma@XizhendeMBP.modem>
2020-10-19 19:12:48 +1100

    减少连接和关闭DB的次数

--- XExport.py
+++ XExport.py
@@ -2,11 +2,8 @@ import pymysql
 import xlwt
 
 
-def export_excel(sql, table_name):
+def export_excel(sql, excel_name, cur):
     # 连接数据库，查询数据
-    host, user, passwd, db = '127.0.0.1', 'root', '123456', 'mysql'
-    conn = pymysql.connect(user=user, host=host, port=3306, passwd=passwd, db=db, charset='utf8')
-    cur = conn.cursor()
     cur.execute(sql)  # 返回受影响的行数
 
     fields = [field[0] for field in cur.description]  # 获取所有字段名
@@ -24,14 +21,18 @@ def export_excel(sql, table_name):
         for col, field in enumerate(data):
             sheet.write(row, col, field)
         row += 1
-    book.save("%s.xls" % table_name)
+    book.save("%s.xls" % excel_name)
 
 
 if __name__ == '__main__':
+    host, user, passwd, db = '127.0.0.1', 'root', '123456', 'mysql'
+    conn = pymysql.connect(user=user, host=host, port=3306, passwd=passwd, db=db, charset='utf8')
+    cur = conn.cursor()
     table_name = 'tpoint'
     query = 'select * from %s' % table_name
     city_name = '青岛市'
     query_1 = "SELECT DISTINCT * FROM tpoint t WHERE t.city_name = '" + city_name + "'"
-    export_excel(query, 'tpoint')
-    export_excel(query_1, '青岛市')
+    export_excel(query, 'tpoint', cur)
+    export_excel(query_1, '青岛市', cur)
+    cur.close()
 
