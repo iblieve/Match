3f47239d26a65140b4dd74ba0050ad665f3789c1
Xizhen Ma <xizhenma@XizhendeMBP.modem>
2020-10-21 04:34:45 +1100

    调用XUtil的封装函数

--- XExport.py
+++ XExport.py
@@ -3,12 +3,14 @@ import xlwt
 from app.utils.XUtils import XUtils
 
 
-def export_excel(sql, excel_name, cur):
+def export_excel(sql, excel_name, cur, conn):
     # 连接数据库，查询数据
-    cur.execute(sql)  # 返回受影响的行数
+    # cur.execute(sql)  # 返回受影响的行数
+    XUtils.execute_sql(p_sql=sql, p_cur=cur, p_conn=conn)
 
     fields = [field[0] for field in cur.description]  # 获取所有字段名
-    all_data = cur.fetchall()  # 所有数据
+    # all_data = cur.fetchall()  # 所有数据
+    success, all_data = XUtils.fetchall_sql(p_sql=sql, p_cur=cur)
 
     # 写入excel
     book = xlwt.Workbook()
@@ -32,15 +34,15 @@ if __name__ == '__main__':
     # host, user, passwd, db = '127.0.0.1', 'root', '123456', 'mysql'
     # conn = pymysql.connect(user=user, host=host, port=3306, passwd=passwd, db=db, charset='utf8')
     # cur = conn.cursor()
-    conn_success, conn, cur = XUtils.db_connect_with_pymysql(p_db_name='mysql')
+    conn_success, conn, cur = XUtils.db_connect_with_pymysql(p_user='root', p_host='127.0.0.1', p_passwd='123456', p_db_name='mysql', p_charset='utf8')
     if conn_success:
         print('sql success connect')
     table_name = 'tpoint'
     query = 'select * from %s' % table_name
     city_name = '青岛市'
     query_1 = "SELECT DISTINCT * FROM tpoint t WHERE t.city_name = '" + city_name + "'"
-    export_excel(query, 'tpoint', cur)
-    export_excel(query_1, '青岛市', cur)
+    export_excel(query, 'tpoint', cur, conn)
+    export_excel(query_1, '青岛市', cur, conn)
     XUtils.db_close(p_cursor=cur, p_conn=conn)
     # cur.close()
     # conn.close()
