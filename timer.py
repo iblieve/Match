24d9e58f917ebfbde1adfd59ab2ba1a2266ab6bf
Xizhen Ma <xizhenma@XizhendeMBP.modem>
2020-12-15 13:42:31 +1100

    添加pause和resume功能

--- timer.py
+++ timer.py
@@ -3,19 +3,31 @@ import time
 import random
 
 
-# 每n秒执行一次
-def timer(n):
+def timer_pause(a, b):
+    for i in range(b):
+        timer_A(a)
+
+
+def timer_A(a):
+    temp = random.random()
+    ticktock(temp)
+    time.sleep(a)
+
+
+def timer_B(a, b):
+    time.sleep(a)
     while True:
         temp = random.random()
-        print(datetime.now().strftime("%Y-%m-%d  %H:%M:%S"))
-        time.sleep(n)
         ticktock(temp)
+        time.sleep(b)
 
 
 def ticktock(n):
+    print(datetime.now().strftime("%Y-%m-%d  %H:%M:%S"))
     print('ticktock')
     print(n)
     pass
 
 
-timer(5)
+timer_pause(5, 5)
+timer_B(30, 5)
