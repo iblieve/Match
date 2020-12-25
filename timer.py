5868108c04d4726aaa5c9a2e42fe410a282d9435
Xizhen Ma <xizhenma@XizhendeMBP.modem>
2020-12-25 11:57:52 +1100

    修改

--- timer.py
+++ timer.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import random
 
 
-k = 0
 def timer_A_pause():
     timer1.cancel()
 
@@ -28,16 +27,31 @@ def fun_timer_B():
     fun_timer_A()
 
 
-if k == 0:
-    fun_timer_A()
-else:
-    timer1 = threading.Timer(k, fun_timer_A)
-    timer1.start()
+class MXZTimer():
+    def __init__(self):
+        pass
+
+    def start(self, p_delay=int):
+        if p_delay == 0:
+            fun_timer_A()
+        else:
+            timer1 = threading.Timer(p_delay, fun_timer_A)
+            timer1.start()
+
+    def stop(self, p_stop_time=int):
+        timer2 = threading.Timer(p_stop_time, timer_A_pause)
+        timer2.start()
+
+    def resume(self, p_pausr_time:int, p_resume_time:int):
+        timer2 = threading.Timer(p_pausr_time, timer_A_pause)
+        timer3 = threading.Timer(p_resume_time, fun_timer_B)
+        timer2.start()
+        timer3.start()
+
 # timer1 = threading.Timer(1, fun_timer_A)
 # timer2 = threading.Timer(30, timer_A_pause)
 # timer3 = threading.Timer(60, fun_timer_B)
 # timer1.start()
 # timer2.start()
 # timer3.start()
-print('123123123123123')
 
